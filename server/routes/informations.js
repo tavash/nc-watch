11f4c5756213a42d1d33381de1a857ef4d276eb7
Robin Bobo <robin.bobo@laposte.net>
2016-05-25 10:17:58 +0200

    Added geolocation module

--- server/routes/informations.js
+++ server/routes/informations.js
@@ -1,43 +1,33 @@
 var express = require('express');
 var router = express.Router();
 var config = require('../keys.json');
-var Bluebox = require('bluebox-ng');
-var bluebox = new Bluebox({});
+var Bluebox = require('bluebox-ng'),
+    bluebox = new Bluebox({});
+var whois = require('whois-json');
+var dns = require('dns');
 
 router.get('/whois', getWhoIs);
 router.get('/shodanHost', getShodanHost);
+router.get('/geolocation', getGeolocation);
 
 //console.log('Modules info:');
 //console.log(JSON.stringify(bluebox.getModulesInfo(), null, 2));
 
+// Récupérer des informations sur le posseceur du domaine
 function getWhoIs(req, res, next) {
-    var moduleOptions = {
-        domain : req.query.domain
-    };
-
-    bluebox.runModule('whois', moduleOptions, function (err, result) {
+    whois(req.query.domain, function (err, result) {
         if (err) {
             console.log('ERROR:');
             console.log(err);
             res.send(err);
-        } else {
-            console.log('RESULT:');
-            console.log(result);
-            result = result.replace(/\n/g,"");
-            result = result.replace(/\r\r/g,"");
-            result = result.split("\r");
-            result = result.slice(0, result.length - 16);
-
-            var resultString = "{";
-            for (var i in result) {
-                result[i] = result[i].split(": ");
-                resultString += '"' + result[i][0] + '":"' + result[i][1] + '",';
-            }
-            resultString = resultString.slice(0, -1);
-            resultString += "}";
-            res.send(JSON.parse(resultString));
         }
-    });
+        else {
+            console.log('RESULT:');
+            console.log(JSON.stringify(result, null, 2))
+
+            res.json(result);
+        }
+    })
 }
 
 function getShodanHost(req, res, next) {
@@ -60,6 +50,48 @@ function getShodanHost(req, res, next) {
             res.json(result);
         }
     });
+}
+
+// Récupère l'addresse ip d'un serveur selon un nom de domaine donné
+function getDnsResolve(domain, res, callback) {
+    dns.resolve(domain, 'A', function (err, addresses) {
+        if (err)
+            res.send(err);
+        else {
+            console.log("IP RESULT : " + addresses)
+            callback(addresses);
+        }
+    });
+}
+
+//Récupère la location du serveur selon un nom de domaine donné
+function getGeolocation(req, res, next) {
+    getDnsResolve(req.query.domain, res, function (serveurIps) {
+        getMultipleGeolocation(serveurIps, function (finalResult) {
+            res.send(finalResult);
+        });
+    });
+}
+
+function getMultipleGeolocation(serveurIps, callback) {
+    var finalResult = [];
+    for (var i in serveurIps) {
+        var moduleOptions = {
+            target: serveurIps[i]
+        };
+        bluebox.runModule('geolocation', moduleOptions, function (err, result) {
+            if (err) {
+                console.log('ERROR:');
+                console.log(err);
+                res.send(err);
+            } else {
+                console.log('RESULT:');
+                console.log(result);
+                finalResult.push(result);
+            }
+        });
+    }
+    callback(finalResult);
 }
 
 module.exports = router;
