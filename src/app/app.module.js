666d48e9b843ee247a63e80243fbe04f78a39afb
florian2412 <florian2412@gmail.com>
2016-05-27 09:18:29 +0200

    Refactoring SHODAN

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -2,6 +2,6 @@
 	'use strict';
 
 	angular
-	.module('ncwatch', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ngMaterial', 'toastr', 'angular-loading-bar']);
+	.module('ncwatch', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ngMaterial', 'toastr', 'angular-loading-bar', 'jsonFormatter']);
 
 })();
--- src/app/tools/shodan/shodan.controller.js
+++ src/app/tools/shodan/shodan.controller.js
@@ -6,18 +6,20 @@
         .controller('ShodanController', ShodanController);
 
     /** @ngInject */
-    function ShodanController(ShodanService) {
+    function ShodanController($scope, ShodanService) {
         var vm = this;
 
         vm.shodan = shodan;
+        vm.isLoad = false;
 
         function shodan(){
             var domain = vm.domain;
 
             ShodanService.shodan(domain).success(function(res){
+                
                 vm.shodanResult = res;
+                vm.isLoad = true;
             });
         }
-
     }
 })();
