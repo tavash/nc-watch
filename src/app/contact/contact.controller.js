12294f43e69324e20d9217154708750f45be39de
Tava Sang <tava.sang@gmail.com>
2016-05-29 13:52:03 +0200

    add contact page

---
+++ src/app/contact/contact.controller.js
@@ -0,0 +1,12 @@
+(function() {
+	'use strict';
+
+	angular.module('ncwatch')
+	.controller('ContactController', ContactController);
+
+	/** @ngInject */
+	function ContactController() {
+		var vm = this;
+
+	}
+})();
